6b845b83d4eb6c715fe692957f09a79401b1ebc3
dixonSolutions <radr40662@gmail.com>
2025-05-29 10:47:15 +1000

    dsjh

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
